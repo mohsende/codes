252ca5872ea904eac588e1b30bd094fe3efc9c30
Mohsen <mm.dehghani109@gmail.com>
2024-05-07 14:58:11 +0330

    Update Newyear time

--- Countdown-timer/script.js
+++ Countdown-timer/script.js
@@ -4,7 +4,7 @@ const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
 
-const newYear = "20 Mar 2024 06:36:00";
+const newYear = "20 Mar 2025 06:36:00";
 
 function countdown() {
     const newYearDate = new Date(newYear);
